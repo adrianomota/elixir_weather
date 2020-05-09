95fc6796d369fa8bd6ccd25b6fad0a25aa752376
Adriano Mota <adrianowsh@hotmail.com>
2020-05-09 14:23:31 -0300

    initial commit

---
+++ lib/elixir_weather.ex
@@ -0,0 +1,10 @@
+defmodule ElixirWeather do
+  def temperatures_of(cities) do
+    coordinator_pid = 
+        spawn(ElixirWeather.Coordinator, :loop, [[], Enum.count(cities)])
+        cities |> Enum.each(fn city ->
+          worker_pid = spawn(ElixirWeather.Worker, :loop, [])
+          send worker_pid, {coordinator_pid, city}
+        end)
+  end
+end
